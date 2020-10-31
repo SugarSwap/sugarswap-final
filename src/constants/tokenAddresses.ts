9abf5c890cf102d6e50eccbae111b40254084a67
shehzad002 <52923030+shehzad002@users.noreply.github.com>
2020-10-31 15:33:45 +0500

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
 //export const sgr = '0xdAE59408fC12c439D6F7607af4261D6756d9E7CF'
-export const sgrv2 = '0xf76649325E47563F9Ea17C004e60a6A9E8285bf4'
+export const sgrv2 = '0x4ed7BB89C2408D8eC4187CDeA2926E240b2688F2'
 export const sgrAddress = '0x7d98cd88574968495BBA8eBB5bFFDE456ff2E35a'
-export const masterChefAddress = '0x08B883491d832835951b06A80D47dBeE2FDDd8b6'
+export const masterChefAddress = '0x156ec8c07d8614e96a32a8b1355F3E181C060f52'
