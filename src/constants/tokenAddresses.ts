da99785c663e352033b9f2a0bc2f3fb7754eb0c6
SugarSwap <72743700+SugarSwap@users.noreply.github.com>
2020-11-02 15:59:48 +0530

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
 //export const sgr = '0xdAE59408fC12c439D6F7607af4261D6756d9E7CF'
-export const sgrv2 = '0xCD2519080796AD7147248b32deb3135d90E54Bea'
+export const sgrv2 = '0x32a3cBB5e0c65e5C03F8bd617Eecec1C46f414AD'
 export const sgrAddress = '0x7d98cd88574968495BBA8eBB5bFFDE456ff2E35a'
-export const masterChefAddress = '0x58f2519Ba1A93F1939c2cD292f840B08f20eE8f1'
+export const masterChefAddress = '0x43bee8d9129307A08f76e7f03f82C13170cf3074'
